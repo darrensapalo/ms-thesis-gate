4366265fc8a7102f4ceaed95656b78e2e00a80d0
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2016-05-04 13:37:31 +0800

    Initial commit.

---
+++ src/Driver.java
@@ -0,0 +1,11 @@
+import gate.Gate;
+
+/**
+ * Created by Darren on 5/4/2016.
+ */
+public class Driver {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        
+    }
+}
